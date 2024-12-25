5e61babb4152684c2c5354e1f1b411b64a3b05c1
Ahmed Gadir <ahmed.gadir@gmail.com>
2024-12-25 16:46:07 +0000

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Connect to the interactive psql shell with psql --username=freecodecamp --dbname=postgres
+
+PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
